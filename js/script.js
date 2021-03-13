592b1406fa5a19d9a7624f90e7f0aed9242f625b
Volodymyr <vova.gubash@gmail.com>
2021-03-13 15:26:55 +0200

    повторив за вчителем

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const box = document.getElementById('box'), 
+   circles = document.getElementsByClassName('circle'),
+   btns = document.getElementsByTagName('button'),
+   oneHeart = document.querySelector('.heart'),
+   hearts = document.querySelectorAll('.heart');   
+
+
+console.dir(box);
+
+box.style.width = '500px';
+box.style.backgroundColor = 'orange';
+
+btns[2].classList.add('active');
+btns[2].classList.remove('active');
+
+const div = document.createElement('div');
+div.classList.add('black');
+box.before(div);
+div.innerHTML = '<h1>Заголовок</h1>';
+console.log(div.innerHTML);
